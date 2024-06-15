a3601ce06dd6e798d477bae6351309aaffd282e4
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-06-15 10:04:56 +0200

    fux ut

--- app/training/[...path]/solutions/5.state-management/12.3.jsx
+++ app/training/[...path]/solutions/5.state-management/12.3.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { cn } from "@/src/utils/cn";
 import { User2, X } from "lucide-react";
 import {
   cloneElement,
@@ -13,36 +12,36 @@ import {
 
 const DialogContext = createContext(null);
 
-const Dialog = ({
-  children,
-  open: openProps = null,
-  setOpen: setOpenProps = null,
-}) => {
+const useDialogContext = () => {
+  const context = useContext(DialogContext);
+
+  if (!context) {
+    throw new Error("useDialogContext must be used within `Dialog`");
+  }
+
+  return context;
+};
+
+const Dialog = ({ children }) => {
   const [open, setOpen] = useState(false);
 
-  const usedOpen = openProps || open;
-  const usedSetOpen = setOpenProps || setOpen;
-
   return (
-    <DialogContext.Provider value={{ open: usedOpen, setOpen: usedSetOpen }}>
+    <DialogContext.Provider value={{ open, setOpen }}>
       {children}
     </DialogContext.Provider>
   );
-};
-
-const useDialogContext = () => {
-  const context = useContext(DialogContext);
-  if (!context) {
-    throw new Error("useDialogContext must be used within a DialogProvider");
-  }
-  return context;
 };
 
 const DialogTrigger = ({ children }) => {
   const { setOpen } = useDialogContext();
 
   if (typeof children !== "string") {
-    return cloneElement(children, { onClick: () => setOpen(true) });
+    return cloneElement(children, {
+      onClick: (e) => {
+        setOpen(true);
+        children.props.onClick?.(e);
+      },
+    });
   }
 
   return (
@@ -52,40 +51,72 @@ const DialogTrigger = ({ children }) => {
   );
 };
 
-const useClickOutside = (ref, handler) => {
-  useEffect(() => {
-    const listener = (event) => {
-      if (ref.current && !ref.current.contains(event.target)) {
-        handler(event);
-      }
-    };
-    document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
+const DialogClose = ({ children }) => {
+  const { setOpen } = useDialogContext();
 
-    return () => {
-      document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
-    };
-  }, [ref, handler]);
+  if (typeof children !== "string") {
+    return cloneElement(children, {
+      onClick: (e) => {
+        setOpen(false);
+        children.props.onClick?.(e);
+      },
+    });
+  }
+
+  return (
+    <button onClick={() => setOpen(false)} className="btn">
+      {children}
+    </button>
+  );
 };
 
 const useEventListener = ({
-  eventName,
   handler,
-  element = window,
   isEnabled = true,
+  type,
+  element = window,
 }) => {
-  useEffect(() => {
-    if (!element) return;
-    if (!isEnabled) return;
+  const handlerRef = useRef(handler);
 
-    const eventListener = (event) => handler(event);
-    element.addEventListener(eventName, eventListener);
+  useEffect(() => {
+    handlerRef.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    if (!isEnabled) {
+      return;
+    }
+    if (!element) {
+      return;
+    }
+
+    const onEvent = (e) => {
+      handlerRef.current(e);
+    };
+
+    element.addEventListener(type, onEvent);
 
     return () => {
-      element.removeEventListener(eventName, eventListener);
+      element.removeEventListener(type, onEvent);
     };
-  }, [eventName, handler, element, isEnabled]);
+  }, [isEnabled, type, element]);
+};
+
+const useClickOutside = (ref, handler) => {
+  const listener = (event) => {
+    if (!ref.current) return;
+    if (ref.current.contains(event.target)) return;
+    handler();
+  };
+
+  useEventListener({
+    handler: listener,
+    type: "mousedown",
+  });
+  useEventListener({
+    handler: listener,
+    type: "touchstart",
+  });
 };
 
 const getFocusableElements = (ref) =>
@@ -95,68 +126,56 @@ const getFocusableElements = (ref) =>
     )
   );
 
-const useFocusTrap = (ref, enabled) => {
-  useEffect(() => {
-    if (!enabled || !ref.current) {
-      return;
-    }
-
-    const focusFirstElement = () => {
-      const focusableElements = getFocusableElements(ref);
-      const firstFocusableElement = focusableElements[0];
-      if (firstFocusableElement) {
-        firstFocusableElement.focus();
-      }
-    };
-    focusFirstElement();
-  }, [ref, enabled]);
-
+const useFocusTrap = (ref, isEnabled) => {
   useEventListener({
-    eventName: "keydown",
-    handler: (e) => {
-      if (e.key !== "Tab") {
+    type: "keydown",
+    isEnabled,
+    handler: (event) => {
+      if (event.key !== "Tab") return;
+
+      const focusableElements = getFocusableElements(ref);
+
+      const activeElement = document.activeElement;
+
+      let nextActiveElementIndex = event.shiftKey
+        ? focusableElements.indexOf(activeElement) - 1
+        : focusableElements.indexOf(activeElement) + 1;
+
+      const toFocusElement = focusableElements[nextActiveElementIndex];
+
+      if (toFocusElement) {
+        console.log("Return nothing");
         return;
       }
 
-      const handleTabKey = (e) => {
-        const focusableElements = getFocusableElements(ref);
-        const activeElement = document.activeElement;
+      nextActiveElementIndex =
+        nextActiveElementIndex < 0 ? focusableElements.length - 1 : 0;
 
-        let nextActiveElementIndex = e.shiftKey
-          ? focusableElements.indexOf(activeElement) - 1
-          : focusableElements.indexOf(activeElement) + 1;
-
-        if (focusableElements[nextActiveElementIndex]) {
-          return;
-        }
-
-        nextActiveElementIndex =
-          nextActiveElementIndex < 0 ? focusableElements.length - 1 : 0;
-
-        focusableElements[nextActiveElementIndex].focus();
-        e.preventDefault();
-        return;
-      };
-
-      handleTabKey(e);
+      focusableElements[nextActiveElementIndex].focus();
+      event.preventDefault();
+      return;
     },
-    isEnabled: enabled,
   });
 };
 
-const DialogContent = ({ children, className = "" }) => {
+const DialogContent = ({ children }) => {
   const { open, setOpen } = useDialogContext();
   const ref = useRef(null);
-  useClickOutside(ref, () => setOpen(false));
+
+  useClickOutside(ref, () => {
+    setOpen(false);
+  });
+
   useEventListener({
-    eventName: "keydown",
     isEnabled: open,
+    type: "keydown",
     handler: (e) => {
       if (e.key === "Escape") {
         setOpen(false);
       }
     },
   });
+
   useFocusTrap(ref, open);
 
   if (!open) return null;
@@ -169,10 +188,7 @@ const DialogContent = ({ children, className = "" }) => {
     >
       <div
         ref={ref}
-        className={cn(
-          "card w-96 bg-base-200 shadow-xl animate-in fade-in-50 slide-in-from-bottom-3",
-          className
-        )}
+        className="card w-96 bg-base-200 shadow-xl animate-in fade-in-50 slide-in-from-bottom-3"
       >
         <div className="card-body">{children}</div>
       </div>
@@ -180,56 +196,30 @@ const DialogContent = ({ children, className = "" }) => {
   );
 };
 
-const DialogClose = ({ children }) => {
-  const { setOpen } = useContext(DialogContext);
-
-  if (typeof children !== "string") {
-    return cloneElement(children, { onClick: () => setOpen(false) });
-  }
-
-  return (
-    <button onClick={() => setOpen(false)} className="btn btn-primary">
-      {children}
-    </button>
-  );
-};
-
 export default function App() {
-  const [open, setOpen] = useState(false);
-
   return (
-    <Dialog open={open} setOpen={setOpen}>
-      <DialogTrigger>
-        <button className="btn btn-primary btn-lg">Open Dialog Now !</button>
-      </DialogTrigger>
-      <DialogContent className="">
-        <DialogClose>
-          <button className="absolute right-4 top-4 flex size-6 items-center justify-center rounded-lg bg-base-100">
-            <X size={12} />
-          </button>
-        </DialogClose>
-
-        <p>What is your name ?</p>
-
-        <label className="input input-bordered flex items-center gap-2">
-          <User2 scale={16} />
-          <input type="text" className="grow" placeholder="Username" />
-        </label>
-        <div className="flex gap-2">
+    <div>
+      <Dialog>
+        <DialogTrigger>
+          <button className="btn btn-primary btn-lg">Open dialog</button>
+        </DialogTrigger>
+        <DialogContent>
           <DialogClose>
-            <button className="btn btn-ghost">Cancel</button>
+            <button className="absolute right-4 top-4 flex size-6 items-center justify-center rounded-lg bg-base-100">
+              <X size={12} />
+            </button>
           </DialogClose>
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              alert("Submit");
-              setOpen(false);
-            }}
-          >
-            Submit
-          </button>
-        </div>
-      </DialogContent>
-    </Dialog>
+          <p>What is your name ?</p>
+          <label className="input input-bordered flex items-center gap-2">
+            <User2 scale={16} />
+            <input type="text" className="grow" placeholder="Username" />
+          </label>
+          <div className="card-actions">
+            {/* 🦁 Ajoute le bouton "Cancel" */}
+            <button className="btn btn-primary">Submit</button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
