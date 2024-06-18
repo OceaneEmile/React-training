4a6963184d21bcf73fb18e440b86861c6de3612e
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-06-18 15:23:15 +0200

    add hooks exe

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { ThemeController } from "./ThemeController";
 
 export const Header = () => {
   return (
-    <header className="sticky max-w-2xl w-full m-auto top-4 flex items-center gap-2 rounded-full border border-neutral-content/20 bg-neutral px-4 py-2 shadow-md">
-      <img className="size-8" src="/beginreact.png" />
+    <header className="sticky top-4 z-10 m-auto flex w-full max-w-2xl items-center gap-2 rounded-full border border-neutral-content/20 bg-neutral px-4 py-2 shadow-md">
+      <img className="size-8" alt="BeginReact Logo" src="/beginreact.png" />
       <Link href="/" className="font-bold text-neutral-content">
         BeginReact
       </Link>
